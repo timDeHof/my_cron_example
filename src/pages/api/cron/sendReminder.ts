1e82fac78f1e8b4ff2f8e7f2b3636faba6460a5c
timDeHof <ttdehof@gmail.com>
2024-04-24 12:25:50 -0400

    refactor(sendReminder.ts): rename handler function to GET for better clarity and consistency The handler function in sendReminder.ts has been renamed to GET to improve clarity and consistency with the HTTP method being used.

--- src/pages/api/cron/sendReminder.ts
+++ src/pages/api/cron/sendReminder.ts
@@ -1,10 +1,7 @@
 import { axiosClient } from "@/lib/api";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-export default async function handler(
-	req: NextApiRequest,
-	res: NextApiResponse,
-) {
+export default async function GET(req: NextApiRequest, res: NextApiResponse) {
 	try {
 		const response = await axiosClient.get("/users/profile/photos");
 		res.status(200).json(response.data);
