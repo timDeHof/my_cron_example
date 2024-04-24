79d5a54e2fce3a743df93ffcee78ca6a6ca70174
timDeHof <ttdehof@gmail.com>
2024-04-24 12:54:03 -0400

    refactor(sendReminder.ts): replace axiosClient with fetch to simplify code and improve consistency The axiosClient import is removed and replaced with the fetch function to simplify the code and improve consistency. The fetch function is used to make a GET request to `http://localhost:5000/api/v1/users/profile/photos` and the response is returned as JSON.

--- src/pages/api/cron/sendReminder.ts
+++ src/pages/api/cron/sendReminder.ts
@@ -1,10 +1,11 @@
-import { axiosClient } from "@/lib/api";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function GET(req: NextApiRequest, res: NextApiResponse) {
 	try {
-		const response = await axiosClient.get("/users/profile/photos");
-		res.status(200).json(response.data);
+		const response = await fetch(
+			`http://localhost:5000/api/v1/users/profile/photos`,
+		);
+		res.status(200).json(response);
 	} catch (error) {
 		if (typeof error === "object" && error !== null && "status" in error) {
 			res.status(500).json({ error: error });
