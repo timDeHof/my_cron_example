18d983f6897a776f67be26033d61b89a1f1fa9d6
timDeHof <ttdehof@gmail.com>
2024-04-24 13:05:57 -0400

    fix(sendReminder.ts): handle error when fetching users data and return the fetched data The code now checks if the response from the fetch request is not ok, and throws an error with a message "Failed to fetch users data". This ensures that any error during the fetch request is properly handled. If the fetch is successful, the fetched data is stored in the usersData variable and returned in the response as { usersData }.

--- src/pages/api/cron/sendReminder.ts
+++ src/pages/api/cron/sendReminder.ts
@@ -5,7 +5,12 @@ export default async function GET(req: NextApiRequest, res: NextApiResponse) {
 		const response = await fetch(
 			`http://localhost:5000/api/v1/users/profile/photos`,
 		);
-		res.status(200).json(response);
+
+		if (!response.ok) {
+			throw new Error("Failed to fetch users data");
+		}
+		const usersData = await response.json();
+		res.status(200).json({ usersData });
 	} catch (error) {
 		if (typeof error === "object" && error !== null && "status" in error) {
 			res.status(500).json({ error: error });
